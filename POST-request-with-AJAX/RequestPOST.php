cf14b638d9745e2a68c12f3faca23d8f780dd82b
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2022-05-07 20:18:56 -0500

    Update RequestPOST.php

--- POST-request-with-AJAX/RequestPOST.php
+++ POST-request-with-AJAX/RequestPOST.php
@@ -1,28 +1,8 @@
 <?php
-header('Content-type: application/json');
-// Si queremos mostrar un contenido JSON:
-// Vamos a mostrar un JSON
-/*
-  $_POST es un array vacío
-  array(0) {
 
-  }
-  var_dump($_POST);
-*/
-
-/*
-
-  // Si queremos saber los headers
-  foreach (getallheaders() as $nombre => $valor) {
-  echo "$nombre: $valor\n";
-  }
-
-*/
-
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-
-// ... aqui hacemos las respestivas validaciones de los datos entrantes
+// php://input solo soporta el método POST
+var_dump($_GET);
+// ... aqui hacemos las respectivas validaciones de los datos entrantes
 
 // ... y aqui devolvemos falso o verdadero según la validación
 echo json_encode(["OK" => true]);
